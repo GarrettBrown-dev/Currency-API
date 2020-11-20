47811dc304feb23b3c38bf018bcea4bbe0da6e1f
Garrett <gman9mm@live.com>
2020-11-20 10:08:47 -0800

    build async function for currency

--- src/main.js
+++ src/main.js
@@ -4,7 +4,14 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 
-function getCurrency(response) {
+$(document).ready(function () {
+  const select = document.querySelectorAll('select');
+  const input = document.querySelectorAll('input');
+  let html = '';
 
+  async function getCurrency() {
+    const data = await CurrencyService.getCurrency();
+    const arrKeys = Object.keys(data.converion_rates);
+    const rates = data.conversion_rates;
 
-}
+  }
