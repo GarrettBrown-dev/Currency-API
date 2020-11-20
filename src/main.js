c96d7548146aabb3672d0e63377772f308008f11
Garrett <gman9mm@live.com>
2020-11-20 10:25:32 -0800

    add html mappings, conversion function and event listeners.

--- src/main.js
+++ src/main.js
@@ -10,8 +10,29 @@ $(document).ready(function () {
   let html = '';
 
   async function getCurrency() {
-    const data = await CurrencyService.getCurrency();
+    const data = await CurrencyService.Currencies();
     const arrKeys = Object.keys(data.converion_rates);
     const rates = data.conversion_rates;
 
+    arrKeys.map(item => {
+      return html += `<option value=${item}>${item}</option>`;
+    })
+
+    for (let i = 0; i < select.length; i++) {
+      select[i].innerHTML = html;
+    }
+
+    function convert(i, j) {
+      input[i].value = input[j].value * rates[select[i].value] / rates[select[j].value];
+    }
+
+    input[0].addEventListener('keyup', () => convert(1, 0));
+
+    input[1].addEventListener('keyup', () => convert(1, 0));
+
+    select[0].addEventListener('change', () => convert(0, 1));
+
+    select[0].addEventListener('change', () => convert(0, 1));
   }
+  getCurrency();
+})
--- src/service/Currency-Services.js
+++ src/service/Currency-Services.js
@@ -1,14 +1,9 @@
 export default class CurrencyService {
-  static getCurrency() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-      .then(function (response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch(function (error) {
-        return Error(error);
-      });
+
+  static async Currencies() {
+    const API_URL = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    return data
   }
 }
