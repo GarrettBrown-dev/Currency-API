c0c0ae976a6987b6e4b55b582665e86ec1c52705
Garrett <gman9mm@live.com>
2020-11-20 14:56:41 -0800

    Finish conversion function.

--- src/main.js
+++ src/main.js
@@ -9,37 +9,15 @@ $(document).ready(function () {
 
   $('#convert').click(function (event) {
     event.preventDefault();
-
-// This is how my old project worked. I re-did it like this as I understand this method much better than how we learned. However I wanted to re-do it in a different way to prove to myself I could do it without aid.
-// $(document).ready(function () {
-//   const select = document.querySelectorAll('select');
-//   const input = document.querySelectorAll('input');
-//   let html = '';
-
-//   async function getCurrency() {
-//     const data = await CurrencyService.Currencies();
-//     const arrKeys = Object.keys(data.converion_rates);
-//     const rates = data.conversion_rates;
-
-//     arrKeys.map(item => {
-//       return html += `<option value=${item}>${item}</option>`;
-//     })
-
-//     for (let i = 0; i < select.length; i++) {
-//       select[i].innerHTML = html;
-//     }
-
-//     function convert(i, j) {
-//       input[i].value = input[j].value * rates[select[i].value] / rates[select[j].value];
-//     }
-
-//     input[0].addEventListener('keyup', () => convert(1, 0));
-
-//     input[1].addEventListener('keyup', () => convert(1, 0));
-
-//     select[0].addEventListener('change', () => convert(0, 1));
-
-//     select[0].addEventListener('change', () => convert(0, 1));
-//   }
-//   getCurrency();
-// })
+    let newExchange;
+    const inputCurrency = $('#input_currency').val();
+    const inputAmount = $('#input_amount').val();
+    $('#exchange_amount').html("Exchange Amount: ");
+    $('#input_amount').val("");
+    CurrencyService.getCurrency()
+      .then(function (response) {
+        newExchange = new ExchangeRates(inputCurrency, inputAmount, response)
+        $('#exchange_amount').append(`${newExchange.exchangeAmount(inputCurrency, inputAmount)} ${inputCurrency}`);
+      });
+  })
+});
