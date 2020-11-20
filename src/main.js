b722e023fe9f393a717edfe95587cb59cea44ef3
Garrett <gman9mm@live.com>
2020-11-20 09:47:02 -0800

    Still working, having trouble remembering how to do... everything for some reason.

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import CurrencyService from './service/Currency-Services.js';
+import $ from 'jQuery';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+
+function getCurrency(response) {
+
+
+}
