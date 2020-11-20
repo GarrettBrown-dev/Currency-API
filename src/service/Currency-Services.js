2773d75f58ba69a2d6e137192d955ebf4303fb62
Garrett <gman9mm@live.com>
2020-11-20 11:05:52 -0800

    Re-add static function.

--- src/service/Currency-Services.js
+++ src/service/Currency-Services.js
@@ -1,10 +1,22 @@
 export default class CurrencyService {
-
-  // Old way. Uses Async functions and awaits.
-  // static async Currencies() {
-  //   const API_URL = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-  //   const res = await fetch(API_URL);
-  //   const data = await res.json();
-  //   return data
-  // }
+  static getCurrency() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return Error(error);
+      })
+  }
 }
+
+// Old way. Uses Async functions and awaits.
+// static async Currencies() {
+//   const API_URL = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+//   const res = await fetch(API_URL);
+//   const data = await res.json();
+//   return data
+// }
