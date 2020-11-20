15d5fed78928db4c751fcf64e9f788fb5917e34e
Garrett <gman9mm@live.com>
2020-11-20 10:28:59 -0800

    Commenting code out. Re-doing it in a different fashion.

--- src/main.js
+++ src/main.js
@@ -4,35 +4,36 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 
-$(document).ready(function () {
-  const select = document.querySelectorAll('select');
-  const input = document.querySelectorAll('input');
-  let html = '';
+// This is how my old project worked. I re-did it like this as I understand this method much better than how we learned. However I wanted to re-do it in a different way to prove to myself I could do it without aid.
+// $(document).ready(function () {
+//   const select = document.querySelectorAll('select');
+//   const input = document.querySelectorAll('input');
+//   let html = '';
 
-  async function getCurrency() {
-    const data = await CurrencyService.Currencies();
-    const arrKeys = Object.keys(data.converion_rates);
-    const rates = data.conversion_rates;
+//   async function getCurrency() {
+//     const data = await CurrencyService.Currencies();
+//     const arrKeys = Object.keys(data.converion_rates);
+//     const rates = data.conversion_rates;
 
-    arrKeys.map(item => {
-      return html += `<option value=${item}>${item}</option>`;
-    })
+//     arrKeys.map(item => {
+//       return html += `<option value=${item}>${item}</option>`;
+//     })
 
-    for (let i = 0; i < select.length; i++) {
-      select[i].innerHTML = html;
-    }
+//     for (let i = 0; i < select.length; i++) {
+//       select[i].innerHTML = html;
+//     }
 
-    function convert(i, j) {
-      input[i].value = input[j].value * rates[select[i].value] / rates[select[j].value];
-    }
+//     function convert(i, j) {
+//       input[i].value = input[j].value * rates[select[i].value] / rates[select[j].value];
+//     }
 
-    input[0].addEventListener('keyup', () => convert(1, 0));
+//     input[0].addEventListener('keyup', () => convert(1, 0));
 
-    input[1].addEventListener('keyup', () => convert(1, 0));
+//     input[1].addEventListener('keyup', () => convert(1, 0));
 
-    select[0].addEventListener('change', () => convert(0, 1));
+//     select[0].addEventListener('change', () => convert(0, 1));
 
-    select[0].addEventListener('change', () => convert(0, 1));
-  }
-  getCurrency();
-})
+//     select[0].addEventListener('change', () => convert(0, 1));
+//   }
+//   getCurrency();
+// })
