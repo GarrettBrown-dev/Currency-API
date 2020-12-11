10bd8bcb82275fe6a7ab50122f7203dc5adce709
Garrett <gman9mm@live.com>
2020-12-10 18:22:28 -0800

    Add further explanation on installation.

--- src/service/Currency-Services.js
+++ src/service/Currency-Services.js
@@ -1,6 +1,6 @@
 export default class CurrencyService {
   static getCurrency() {
-    return fetch(`https://v6.eexchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function (response) {
         return response.json();
       })
